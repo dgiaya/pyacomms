d398d3ef560785b9c1f0f9853bf27e5a154ab8ec
Dennis Giaya <dgiaya@whoi.edu>
2019-05-13 15:49:11 -0400

    connection_utilities.py: fixed import urlparse -> urllib.parse in python3

--- acomms/modem_connections/connection_utilities.py
+++ acomms/modem_connections/connection_utilities.py
@@ -1,9 +1,9 @@
-import urlparse
+import urllib
 
 def connection_class_from_name(name):
     pass
 
 def connection_from_string(connection_string):
-    parsed = urlparse.urlparse(connection_string)
+    parsed = urllib.parse(connection_string)
     pass
     # Look at the scheme to decide what connection type to use.
