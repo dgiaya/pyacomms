7da7389bd45c5116ec43645797c06bc384544c54
Dennis Giaya <dgiaya@whoi.edu>
2019-05-13 13:55:59 -0400

    messageparams.py: don't need to import exceptions. Fixed whitespace for PEP

--- acomms/messageparams.py
+++ acomms/messageparams.py
@@ -1,18 +1,18 @@
 from binascii import hexlify, unhexlify
-import exceptions
 
 def data_from_hexstring(hexstring):
     databytes = bytearray()
     try:
         databytes.extend([ord(c) for c in unhexlify(hexstring)])
     #Catch Odd-length String Error
-    except exceptions.TypeError:
+    except TypeError:
         pass
     
     return databytes
 
 def hexstring_from_data(databytes):
     return hexlify(bytes(databytes))
+
 
 class CycleInfo(object):
     def __init__(self, src, dest, rate_num, ack=False, num_frames=None):
@@ -29,7 +29,8 @@ class CycleInfo(object):
     # This allows us to see if two cycleinfo objects match
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
-        
+
+
 class DrqParams(object):
     def __repr__(self):
         return "SRC: {} DST: {} ACK: {} NUMBYTES: {} FRAME#: {}".format(self.src,self.dest,self.ack,self.num_bytes,self.frame_num)
@@ -40,7 +41,8 @@ class DrqParams(object):
         self.ack = bool(ack)
         self.num_bytes = int(num_bytes)
         self.frame_num = int(frame_num)
-        
+
+
 class Ack(object):
     def __repr__(self):
         return "SRC: {} DST: {} FRAME#: {}".format(self.src,self.dest,self.frame_num)
@@ -50,7 +52,6 @@ class Ack(object):
         self.dest = int(dest)
         self.ack = bool(ack)
         self.frame_num = int(frame_num)
-
 
 
 class DataFrame(object):
@@ -68,6 +69,7 @@ class DataFrame(object):
     def __repr__(self):
         return "SRC: {} DST: {} ACK: {} FRAME#: {} BAD_CRC: {} DATA: {}".format(self.src,self.dest,self.ack,self.frame_num, self.bad_crc, repr(self.data))
 
+
 class CCPGT(object):
     def __init__(self, txfreq, txcode,timeout_ms,  codelen, rxfreq, rxcode1, rxcode2, rxcode3, rxcode4, reserved1=0,reserved2=0):
         self.txfreq = txfreq
@@ -84,6 +86,7 @@ class CCPGT(object):
 
         def __repr__(self):
             return "TXHz: {} RXHz: {} TXCODE: {}".format(self.txfreq,self.rxfreq,self.txcode)
+
 
 class Packet(object):
     def __init__(self, cycleinfo, frames=None):
@@ -113,18 +116,19 @@ class PacketRate(object):
         
     maxpacketsize = property(getpacketsize)
 
-Rates = {0:PacketRate('FH-FSK', 0, 32, 1),
-         1:PacketRate('BCH 128:8', 1, 64, 3),
-         2:PacketRate('DSS 1/15 (64B frames)', 2, 64, 3),
-         3:PacketRate('DSS 1/7', 3, 256, 2),
-         4:PacketRate('BCH 64:10', 4, 256, 2),
-         5:PacketRate('Hamming 14:9', 5, 256, 8),
-         6:PacketRate('DSS 1/15 (32B frames)', 6, 32, 6)}
 
-FDPMiniRates = {1:PacketRate('BCH 128:8', 1, 64, 1),
-            3:PacketRate('BCH 64:10', 3, 60, 1),
-            5:PacketRate('Hamming 14:9', 5, 55, 1)}
-FDPDataRates = {1:PacketRate('BCH 128:8', 1, 64, 3),
-                3:PacketRate('BCH 64:10', 3, 100, 1),
-                5:PacketRate('Hamming 14:9', 5, 256, 8)}
-LDRRates = {7:PacketRate('BCH 64:10', 1, 260, 1)}
+Rates = {0: PacketRate('FH-FSK', 0, 32, 1),
+         1: PacketRate('BCH 128:8', 1, 64, 3),
+         2: PacketRate('DSS 1/15 (64B frames)', 2, 64, 3),
+         3: PacketRate('DSS 1/7', 3, 256, 2),
+         4: PacketRate('BCH 64:10', 4, 256, 2),
+         5: PacketRate('Hamming 14:9', 5, 256, 8),
+         6: PacketRate('DSS 1/15 (32B frames)', 6, 32, 6)}
+
+FDPMiniRates = {1: PacketRate('BCH 128:8', 1, 64, 1),
+                3: PacketRate('BCH 64:10', 3, 60, 1),
+                5: PacketRate('Hamming 14:9', 5, 55, 1)}
+FDPDataRates = {1: PacketRate('BCH 128:8', 1, 64, 3),
+                3: PacketRate('BCH 64:10', 3, 100, 1),
+                5: PacketRate('Hamming 14:9', 5, 256, 8)}
+LDRRates = {7: PacketRate('BCH 64:10', 1, 260, 1)}
